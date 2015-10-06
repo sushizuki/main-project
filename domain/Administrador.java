50e2de0127742d2ddadc51de381f4e37ae045591
Jessica Suzuki <suzuki_jessica@hotmail.com>
2015-10-06 09:51:52 -0300

    Add get and sets

--- domain/Administrador.java
+++ domain/Administrador.java
@@ -1,3 +1,5 @@
 public class Administrador extends Usuario {
 
-}
+	}
+
+
--- domain/Cliente.java
+++ domain/Cliente.java
@@ -1,11 +1,43 @@
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Cliente extends Usuario {
+
+	public Cliente(ArrayList<Pedido> pedido, Mensagem mensagem, Calendar dataEnviada) {
+		super();
+		this.pedido = pedido;
+		this.mensagem = mensagem;
+		this.dataEnviada = dataEnviada;
+	}
 
 	private ArrayList<Pedido> pedido;
 
 	private Mensagem mensagem;
 	
 	private Calendar dataEnviada;
+
+	public ArrayList<Pedido> getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(ArrayList<Pedido> pedido) {
+		this.pedido = pedido;
+	}
+
+	public Mensagem getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(Mensagem mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Calendar getDataEnviada() {
+		return dataEnviada;
+	}
+
+	public void setDataEnviada(Calendar dataEnviada) {
+		this.dataEnviada = dataEnviada;
+	}
 
 }
