19b58bbb0de3b193c2f473ced2286b85616e7840
Atilla Gallio <atilla.gallio@gmail.com>
2015-10-08 22:52:16 -0300

    Chances to user Controller

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,5 +1,25 @@
 package controller;
-
-public class UserController {
-
+ 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+ 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import model.UserDAO;
+import domain.User;
+ 
+public class UserController extends HttpServlet {
+private static final long serialVersionUID = 1L;
+private UserDAO dao;
+ 
+public UserController() {
+super();
+dao = new UserDAO();
 }
+} 
