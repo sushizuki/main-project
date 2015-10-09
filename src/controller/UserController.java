9b8346397cea06e93dcca958cc6aecff4168f0e8
Atilla Gallio <atilla.gallio@gmail.com>
2015-10-08 23:11:04 -0300

    User Controller Working

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -17,15 +17,16 @@ import domain.Product;
 import domain.User;
  
 public class UserController extends HttpServlet {
-private static final long serialVersionUID = 1L;
-private UserDAO dao;
- 
-public UserController() {
-
 	
-	//Choose action then forward to right method
-			createUser(request, response);
-		}
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request,
+		HttpServletResponse response) throws ServletException, IOException {
+				
+		//Choose action then forward to right method
+		createUser(request, response);
+	}
 		
 		private void createUser(HttpServletRequest request,
 				HttpServletResponse response) throws IOException{
@@ -55,7 +56,6 @@ public UserController() {
 	        out.println("</html>");
 		}
 
-	}
 
 }
-} 
+
