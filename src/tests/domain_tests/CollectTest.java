aa1b09ce74a2cb9662aa39c408c26834153fa388
Jessica Suzuki <suzuki_jessica@hotmail.com>
2016-06-05 15:00:36 -0300

    Creating collect test class

---
+++ src/tests/domain_tests/CollectTest.java
@@ -0,0 +1,28 @@
+package tests.domain_tests;
+
+import java.util.Calendar;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.Address;
+
+public class CollectTest {
+
+	@Test
+	public void test(){	
+		Assert.assertTrue(true);
+	}
+	
+	//Mudar o tipo e verificar o teste
+	String sushizuki_location;
+	int dateTime;
+
+	@Before
+	public void setUp() {
+		
+		sushizuki_location = "";
+		dateTime = 90;
+	}
+}
