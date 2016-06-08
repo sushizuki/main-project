37a6b677eafa2e66ebfcbbb01da4ec2caea3bb6a
Pedro Alcantara <ptralcantara@gmail.com>
2016-06-08 17:02:14 -0300

    Creating tests signatures in ProductTest

--- src/tests/domain_tests/ProductTest.java
+++ src/tests/domain_tests/ProductTest.java
@@ -10,17 +10,81 @@ import exceptions.InvalidFormatException;
 
 
 public class ProductTest {
+	private String nameProduct;
+	private String descriptionOfProduct;
+	private double priceOfProduct;
+	private String imageURL;
+	private String categoryOfProduct;
+	private String extra;
 
 	@Before
 	public void setUp() throws Exception {
+		this.nameProduct = "Sushi";
+		this.descriptionOfProduct = "Sushi Sushi Sushi";
+		this.priceOfProduct = 10.10;
+		this.imageURL = "sushizukiSushi.jpg";
+		this.categoryOfProduct = "1";
+		this.extra = "cebolinha";
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	
+	@Test
+	public void testValidPrice() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvalidPrice() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	public void test() {
+	public void testValidDescription() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEmptyDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidName() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEmptyName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidImageURL() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEmptyImageURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidCategoryString() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvalidCategoryString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidCategoryId() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvalidCategoryId() {
 		fail("Not yet implemented");
 	}
 
