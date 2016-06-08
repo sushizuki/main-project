4169f199b950fac7fdc1d62fce16587f9d9e5608
Pedro Alcantara <ptralcantara@gmail.com>
2016-06-08 16:27:10 -0300

    Creating ProductTest

---
+++ src/tests/domain_tests/ProductTest.java
@@ -0,0 +1,27 @@
+package tests.domain_tests;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import domain.Product;
+import exceptions.EmptyFieldException;
+import exceptions.InvalidFormatException;
+
+
+public class ProductTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
